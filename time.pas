476f0aa933162fc034f4333f135bec45996c55da
Кирилл Балдин <baldinkirill58@gmail.com>
2023-09-30 09:29:44 +0300

    Добавил новый файл time.pas

---
+++ time.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var year := ReadInteger('Введите год:');
+  if (year mod 4 = 0) and (not((year mod 100 = 0) and (year mod 400 <> 0))) then
+    print('Високосный год')
+  else
+    print('Не високосный год')
+end.
